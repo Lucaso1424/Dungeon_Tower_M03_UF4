75b6f0a64eb42492fe544cb4c190c40f43d9ad33
Lucas Padilla <lucaspadilla648@gmail.com>
2021-01-24 18:06:42 +0100

    lph - creacio de classes habitacio, personatges mapa i monstre

--- src/Personatges/Jugador.java
+++ src/Personatges/Jugador.java
@@ -3,12 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Projecte;
+package Personatges;
 
 /**
  *
  * @author lucas
  */
 public class Jugador {
-    
+    private int vida;
+    private final static int vida_maxima = 10;
+    private int atac;
+    private int defensa;
 }
